37f6687676a8155aaaa0a8839df36828d9511505
baeroe <rafael.hingerl@hotmail.com>
2018-07-05 17:46:54 +0200

    enable textfiel icon reingemacht

--- app/src/main/java/comfabiankockpflegi3000/github/pflegi3000/activities/show_patient_activity/ShowPatientActivity.java
+++ app/src/main/java/comfabiankockpflegi3000/github/pflegi3000/activities/show_patient_activity/ShowPatientActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,12 @@ public class ShowPatientActivity extends AppCompatActivity {
     private Button backBtn;
     private Toolbar theToolbar;
     private ShowPatientButtonListener btnListener;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.show_patient_bar, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
