3a261cf389418d1b166e59a9f9962cf162915e04
baeroe <rafael.hingerl@hotmail.com>
2018-07-06 18:29:47 +0200

    added wiews in patientFragment

--- app/src/main/java/comfabiankockpflegi3000/github/pflegi3000/activities/show_patient_activity/ShowPatientFragmente/PatientFragment.java
+++ app/src/main/java/comfabiankockpflegi3000/github/pflegi3000/activities/show_patient_activity/ShowPatientFragmente/PatientFragment.java
@@ -8,15 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import comfabiankockpflegi3000.github.pflegi3000.R;
 
 public class PatientFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
+    //Editierbare Felder
     private EditText firstNameText, lastNameText;
+    private RadioGroup rgGender;
+    private EditText insuranceNrText;
+    private Spinner insuranceTypeSpinner;
+
+    //Nicht editierbare Felder
     private TextView firstNameView, lastNameView;
+    private TextView genderView, insuranceNrView, insuranceTypeView;
+
     private View view;
 
     public PatientFragment() {}
@@ -37,15 +50,35 @@ public class PatientFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_patient, container, false);
 
+        //editierbare Felder
         firstNameText = view.findViewById(R.id.text_firstname);
         firstNameText.setText("Hans");
+
         lastNameText = view.findViewById(R.id.text_lastname);
         lastNameText.setText("Peter");
 
+        rgGender = view.findViewById(R.id.radio_gender);
+
+        insuranceNrText = view.findViewById(R.id.text_insuranceNr);
+        insuranceNrText.setText("12345678");
+
+        insuranceTypeSpinner = view.findViewById(R.id.spinner_insuranceType);
+
+        //nicht editierbare Felder
         firstNameView = view.findViewById(R.id.view_firstname);
         firstNameView.setText("Hans");
+
         lastNameView = view.findViewById(R.id.view_lastname);
         lastNameView.setText("Peter");
+
+        genderView = view.findViewById(R.id.view_gender);
+        genderView.setText("Männlich");
+
+        insuranceNrView = view.findViewById(R.id.view_insuranceNr);
+        insuranceNrView.setText("12345678");
+
+        insuranceTypeView = view.findViewById(R.id.view_insuranceType);
+        insuranceTypeView.setText("AOK");
 
         return view;
 
@@ -80,16 +113,37 @@ public class PatientFragment extends Fragment {
 
     /*----------getter----------*/
 
+    //nicht editierbare Felder
     public EditText getLastNameText() {
         return lastNameText;
     }
     public EditText getFirstNameText() {
         return firstNameText;
     }
+    public RadioGroup getRgGender() {
+        return rgGender;
+    }
+    public EditText getInsuranceNrText() {
+        return insuranceNrText;
+    }
+    public Spinner getInsuranceTypeSpinner() {
+        return insuranceTypeSpinner;
+    }
+
+    //editierbare Felder
     public TextView getLastNameView() {
         return lastNameView;
     }
     public TextView getFirstNameView() {
         return firstNameView;
+    }
+    public TextView getGenderView() {
+        return genderView;
+    }
+    public TextView getInsuranceNrView() {
+        return insuranceNrView;
+    }
+    public TextView getInsuranceTypeView() {
+        return insuranceTypeView;
     }
 }
--- app/src/main/java/comfabiankockpflegi3000/github/pflegi3000/controller/show_patient_controller/ControllerPatientFragment.java
+++ app/src/main/java/comfabiankockpflegi3000/github/pflegi3000/controller/show_patient_controller/ControllerPatientFragment.java
@@ -25,17 +25,29 @@ public class ControllerPatientFragment {
 
         activity.getFirstNameText().setVisibility(View.VISIBLE);
         activity.getLastNameText().setVisibility(View.VISIBLE);
+        activity.getRgGender().setVisibility(View.VISIBLE);
+        activity.getInsuranceNrText().setVisibility(View.VISIBLE);
+        activity.getInsuranceTypeSpinner().setVisibility(View.VISIBLE);
 
         activity.getFirstNameView().setVisibility(View.INVISIBLE);
         activity.getLastNameView().setVisibility(View.INVISIBLE);
+        activity.getGenderView().setVisibility(View.INVISIBLE);
+        activity.getInsuranceNrView().setVisibility(View.INVISIBLE);
+        activity.getInsuranceTypeView().setVisibility(View.INVISIBLE);
     }
 
     public void EditToView() {
 
         activity.getFirstNameText().setVisibility(View.INVISIBLE);
         activity.getLastNameText().setVisibility(View.INVISIBLE);
+        activity.getRgGender().setVisibility(View.INVISIBLE);
+        activity.getInsuranceNrText().setVisibility(View.INVISIBLE);
+        activity.getInsuranceTypeSpinner().setVisibility(View.INVISIBLE);
 
         activity.getFirstNameView().setVisibility(View.VISIBLE);
         activity.getLastNameView().setVisibility(View.VISIBLE);
+        activity.getGenderView().setVisibility(View.VISIBLE);
+        activity.getInsuranceNrView().setVisibility(View.VISIBLE);
+        activity.getInsuranceTypeView().setVisibility(View.VISIBLE);
     }
 }
